d06f96f56f7fc7140b108ff9308efb78c84f7a88
Abhishek-data <abhishekkr1915@gmail.com>
2022-11-21 20:48:41 +0530

    filter

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const form = document.getElementById('addForm');
 const itemlist = document.getElementById('items');
-
+const filter = document.getElementById('filter');
 // for submit events
 form.addEventListener('submit', additem);
 itemlist.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
 // additem funtion
 function additem(e){
     e.preventDefault();
     //get input value
     const newitem = document.getElementById('item');
+    const newitem2 = document.getElementById('item2');
     // create new li element
     const li = document.createElement('li');
     li.className = 'list-group-item';
     // add text node with input value
     li.appendChild(document.createTextNode(newitem.value));
+    li.appendChild(document.createTextNode(newitem2.value));
     //create del button
     const deleteBtn = document.createElement('button');
     // add class to del button
@@ -33,6 +36,25 @@ function removeItem(e) {
             itemlist.removeChild(li);
         }
     }
+}
+
+// filter items
+function filterItems(e) {
+    //convert text to lowercase
+    const text = e.target.value.toLowerCase();
+    const items = itemlist.getElementsByTagName('li');
+    //convert to aray
+    Array.from(items).forEach(function(item) {
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLocaleLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        }else {
+            item.style.display = 'none';
+        }
+    })
+
+
+
 }
 
 
