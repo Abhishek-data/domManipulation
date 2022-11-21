fc43ba3101699c02d62e388852f5c02f790b6814
Abhishek-data <abhishekkr1915@gmail.com>
2022-11-21 17:53:45 +0530

    Added my project

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-// GetElement by class name
+const itemlist = document.querySelector('#items');
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor = 'red';
+// console.log(itemlist.parentNode.parentNode.parentNode.parentNode);
+
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor = 'red';
+// console.log(itemlist.parentElement.parentElement.parentElement);
+// childnode
+// console.log(itemlist.childNodes);
+// console.log(itemlist.children[1]);
+// itemlist.children[1].style.backgroundColor = 'blue';
+
+// //first child
+// console.log(itemlist.firstChild)
+// // first elementchild
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.textContent = 'gulabjamun';
+
+// // lasst elementchild
+// console.log(itemlist.lastChild)
+// console.log(itemlist.lastElementChild);
+// itemlist.lastElementChild.textContent = 'gulabjamun';
+// itemlist.lastChild.textContent = 'gulabjamun';
+//next sibling
+// console.log(itemlist.nextSibling)
+// console.log(itemlist.nextElementSibling)
+// console.log(itemlist.previousSibling);
+// console.log(itemlist.previousElementSibling);
+// itemlist.previousElementSibling.style.color = 'green'
+// createElement
 
 
-const items = document.getElementsByClassName('list-group-item')
-console.log(items[1]);
-items[1].textContent = 'hello';
-items[1].style.backgroundColor = 'green';
-for(i=0; i <items.length; i++) {
-    items[i].style.fontWeight = 'bold';
-}
+//create a div
+const newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute('title', 'hello div');
+
+
+// create text node
+const newDivText = document.createTextNode('hello world');
+//add text to div
+newDiv.appendChild(newDivText);
+
+const container = document.querySelector('header .container');
+const h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+newDiv.style.fontSize = '30px'
+container.insertBefore(newDiv, h1);
+
 
