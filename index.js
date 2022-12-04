c6b5ad5bf0184065e5cbc3a00c72fa6b25d9e0c7
Abhishek-data <abhishekkr1915@gmail.com>
2022-12-04 09:45:58 +0530

    arrow funtion

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-// funtion currying
-let multiply = function(a, b){
-    console.log(a * b)
-}
 
-let multiplyByTwo = multiply.bind(this, 2);
-multiplyByTwo(5)
-
-let multiplyByThree = multiply.bind(this, 3);
-multiplyByThree(5)
-
-// funtion currying with the help of closure
-
-let multiply1 = function(a){
-    return function(b){
-        console.log(a * b)
+class student{
+    static count = 0;
+    constructor(name, age, phoneNumber, boardMarks){
+        this.name = name,
+        this.age = age, 
+        this.phoneNumber = phoneNumber,
+        this.boardMarks = boardMarks,
+        student.countStudent()
+    }
+    static countStudent(){
+        return (student.count ++)
+    }
+    eligible(){
+        if(this.boardMarks > 40){
+            console.log("eligible")
+        }else if(this.boardMarks<40){
+            console.log("not eligible")
+        } 
+    }
+    setPlacementAge(minPlacementAge){
+        return (minMarks) => {
+            if(this.minMarks > minMarks && this.boardMarks > minPlacementAge){
+                console.log(`${this.name} is eligible for placement`)
+            }
+            else{
+                console.log(`${this.name} is not eligible for placement`)
+            }
+        }
     }
 }
 
-let multiplyByFour = multiply1(4);
-multiplyByFour(5)
+
+const student1 = new student('sonu', 12, 12345, 30)
+const student2 = new student('sonu', 12, 12345, 50)
+const student3 = new student('sonu', 12, 12345, 50)
+const student4 = new student('sonu', 12, 12345, 50)
+const student5 = new student('sonu', 12, 12345, 50)
+
+student1.setPlacementAge(10)(50)
+
+student1.eligible()
+console.log(student.countStudent())
+
